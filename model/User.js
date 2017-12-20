d93d50a80e93b1cab0d0fc0dd5c63f8b9ad4b5f6
Shachy Rivas <shachyj.rivas@gmail.com>
2017-12-20 12:00:52 -0500

    Add password and username properties to user model

--- model/User.js
+++ model/User.js
@@ -10,6 +10,7 @@ const UserSchema = new Schema({
   name: { type: String, required: true },
   username: { type: String, required: true },
   email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', UserSchema);
