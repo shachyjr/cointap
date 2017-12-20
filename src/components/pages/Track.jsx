5c692b7976a1fa1cde785484ef9521f49861cfbd
Shachy Rivas <shachyj.rivas@gmail.com>
2017-12-20 09:43:54 -0500

    Add track componant that will hold page information for tracking a currency etc

---
+++ src/components/pages/Track.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Track extends Component {
+  render() {
+    return [
+      <h1 key="track-heading">THIS IS THE TRACKING PAGE</h1>,
+    ];
+  }
+}
+
+export default Track;
