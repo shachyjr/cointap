8a9c842f8ea61f0e4df779db3e590a0ad7110297
Shachy Rivas <shachyj.rivas@gmail.com>
2017-12-20 14:45:45 -0500

    Create register page

---
+++ src/components/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Register = ({ handleRegister, nameChange, userNameChange, emailChange, passwordChange }) => (
+  <form onSubmit={handleRegister}>
+    <input type="text" onChange={nameChange} placeholder="Name"></input>
+    <input type="text" onChange={userNameChange} placeholder="username"></input>
+    <input type="text" onChange={emailChange} placeholder="email"></input>
+    <input type="password" onChange={passwordChange} placeholder="password"></input>
+    <input type="submit" value="Login"></input>
+  </form>
+)
+
+export default Register;
