5099b697a3ab3c65ba50a1bea55c159d7d0290b3
Shachy Rivas <shachyj.rivas@gmail.com>
2017-12-20 12:00:32 -0500

    Create add and verify methods for user model, which will add a user to the db & verify ai user and password -- complete with encrypting

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,18 +1,55 @@
-const mongoose = require('mongoose');
-const User = require('../model/User.js');
-const { MONGO_URI } = require('../env.js');
+const bcrypt = require('bcrypt');
 
-mongoose.connect(MONGO_URI, { useMongoClient: true }, () => {
-  console.log('Connection to database successful');
-});
+const User = require('../model/User.js');
+const { SALT_ROUNDS } = require('../utils/env.js');
 
 const UserController = {};
 
-UserController.createUser = (req, res) => {
-  User.create({}, (err, user) => {
-
+UserController.add = (req, res) => {
+  // check if user name already exists
+  User.find({ username: req.body.username }, (err, user) => {
+    if (err) return res.sendStatus(500);
+    if (user) {
+      res.status = 409;
+      res.write('Username already exists');
+      return res.end();
+    }
   });
-}
+
+  // encrypt password
+  bcrypt.hash(req.body.password, SALT_ROUNDS, (err, hash) => {
+    if (err) throw new Error(err);
+
+    // create user and store in db
+    User.create({
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    }, (dbErr, user) => {
+      if (dbErr) return res.sendStatus(500);
+      res.status = 200;
+      res.write(user);
+      return res.end();
+    });
+  });
+};
+
+UserController.verify = (req, res) => {
+  User.find({ username: req.body.username }, (dbErr, user) => {
+    if (dbErr) return res.sendStatus(500);
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (!user || !result) {
+        res.status(401);
+        res.write('Username or password is incorrect');
+        return res.end();
+      }
+      res.status = 200;
+      res.write(user);
+      return res.end();
+    });
+  });
+};
 
 module.exports = UserController;
 
