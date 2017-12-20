6030e17060d029e3a0555f40fca6a35352477fe5
Shachy Rivas <shachyj.rivas@gmail.com>
2017-12-20 11:44:41 -0500

    Move connection method to mongoose in this file:

--- model/User.js
+++ model/User.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
+const { MONGO_URI } = require('../utils/env.js');
+
+mongoose.connect(MONGO_URI, { useMongoClient: true }, () => {
+  console.log('Connection to database successful');
+});
 
 const UserSchema = new Schema({
   name: { type: String, required: true },
+  username: { type: String, required: true },
   email: { type: String, required: true },
 });
 
